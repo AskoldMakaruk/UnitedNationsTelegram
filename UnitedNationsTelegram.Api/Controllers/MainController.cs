7f2fa6223a653ad0605683f0e636c8b1b6c9898f
Askold Makaruk <askoldmakaruk@gmail.com>
2022-04-20 02:29:07 +0300

    logs

--- UnitedNationsTelegram.Api/Controllers/MainController.cs
+++ UnitedNationsTelegram.Api/Controllers/MainController.cs
@@ -9,15 +9,18 @@ namespace UnitedNationsTelegram.Api.Controllers;
 public class MainController : ControllerBase
 {
     private readonly MembersService _membersService;
+    private readonly ILogger<MainController> _logger;
 
-    public MainController(MembersService membersService)
+    public MainController(MembersService membersService, ILogger<MainController> logger)
     {
         _membersService = membersService;
+        _logger = logger;
     }
 
     [HttpGet]
     public async Task<JsonResult> Get(long chatId)
     {
+        _logger.LogInformation("Get members {ChatId}", chatId);
         var member = await _membersService.GetMember(chatId);
         return new JsonResult(await _membersService.GetChatMembers(member.ChatId));
     }
